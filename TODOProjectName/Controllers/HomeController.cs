0a083ded586cb637ebe038d6cc2a7b3dff717bf7
Jeff Chiu <jwchiu82@gmail.com>
2020-12-16 09:42:13 -0800

    make changes to homecontroller.cs file

--- TODOProjectName/Controllers/HomeController.cs
+++ TODOProjectName/Controllers/HomeController.cs
@@ -1,19 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-using TODOProjectName.Models;
 
-namespace TODOProjectName.Controllers
+namespace ToDoList.Controllers
 {
     public class HomeController : Controller
     {
-        [Route("/input")] //route for input form
-        public ActionResult Input() { return View();}
 
-        [Route("/output")] //out put route for taking variables to RAZOR
-        public ActionResult Output(//define method parameters)
+        [HttpGet("/")]
+        public ActionResult Index()
         {
-            //InputVariable madLibVariable = new InputVariable();
-            //madLibVariable.Country = country;
-            //return View(madLibVariable);
+            return View();
         }
+
     }
 }
