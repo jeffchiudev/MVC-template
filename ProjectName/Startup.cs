0230ec6370bdd3778922a27e407097310becb3b3
Jeff Chiu <jwchiu82@gmail.com>
2020-12-18 08:13:57 -0800

    updates

--- ProjectName/Startup.cs
+++ ProjectName/Startup.cs
@@ -34,6 +34,8 @@ namespace TODOProjectName
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
 
+            app.UseStaticFiles();
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Oopsie Doodle!");
